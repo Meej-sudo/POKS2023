88c19133d4d252c1e7765ded6cda109f29d3cfb4
Meej-sudo <64920470+Meej-sudo@users.noreply.github.com>
2023-11-21 13:55:10 +0100

    Create Naloga1.c

---
+++ Vaja5/Naloga1.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <semaphore.h>
+
+void fun1();
+
+int main (){
+  pthread_t thread1, thread2;
+  char *msg1 = "1";
+  char *msg2 = "2";
+
+  pthread_create(&thread1, NULL, (void *) fun1, (void*) msg1);
+  pthread_create(&thread2, NULL, (void *) fun1, (void*) msg2);
+
+  pthread_join(thread1, NULL);
+  pthread_join(thread2, NULL);
+  printf("Konec  \n");
+  return (1);
+}
+
+void fun1(void * ptr){
+  char *msg;
+  msg = (char *) ptr;
+  int i,j;
+  int b;
+  for (i=0; i< 40; i++){
+    printf("Thread %s i=%d ", msg, i);
+    for (j=0;j<60; j++){
+      printf("%s", msg);
+  
+      for(b=0; b < 1000000; b++){ // 1000000 -> ~ <1s
+        int a = 100*54/3*123123/433 - 111;
+      }
+    }
+    printf("\n");
+    for(b=0; b < 10000000; b++){ // ~0.5s
+      int a = 100*54/3*123123/433 - 111;
+    }
+  }
+}
